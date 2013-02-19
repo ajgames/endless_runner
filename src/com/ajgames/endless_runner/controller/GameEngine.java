0d3f1c877eae3206e18fcec04583b3a5ffc68ced
Adam Berg <adam@xyzdigital.com>
2013-02-19 13:46:10 -0800

    Refactor MainGamePanel to GameEngine
    This is where the main set up stuff should go.  ie. all of the model,
    view, and controller initializations.  This also holds the main update
    and render functions.

--- src/com/ajgames/endless_runner/controller/GameEngine.java
+++ src/com/ajgames/endless_runner/controller/GameEngine.java
@@ -1,53 +1,72 @@
-package com.ajgames.endless_runner;
+package com.ajgames.endless_runner.controller;
 
 import org.jbox2d.dynamics.World;
 
 import android.app.Activity;
 import android.content.Context;
 import android.graphics.Canvas;
-import android.graphics.Color;
-import android.graphics.Paint;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
-public class MainGamePanel extends SurfaceView implements
+import com.ajgames.endless_runner.MainThread;
+import com.ajgames.endless_runner.model.Physics;
+import com.ajgames.endless_runner.model.Platform;
+import com.ajgames.endless_runner.model.Runner;
+import com.ajgames.endless_runner.view.IRenderer;
+import com.ajgames.endless_runner.view.PlatformRenderer;
+import com.ajgames.endless_runner.view.RunningGameRenderer;
+
+public class GameEngine extends SurfaceView implements
 		SurfaceHolder.Callback
 {
-	private static final String TAG = MainGamePanel.class.getSimpleName();
+	private static final String TAG = GameEngine.class.getSimpleName();
 
 	private World world;
 	private Runner runner;
+	private Platform platforms[];
+	
+	private IRenderer renderer;
+
+	private PlatformController platformController;
+
 	private PlatformRenderer platformRenderer;
 
-	private String avgFps;
 	private MainThread mainThread;
 
-	public MainGamePanel( Context context )
+	public GameEngine( Context context )
 	{
 		super( context );
 		// adding the callback (this) to the surface holder to intercept events
-		getHolder().addCallback( this );
+		this.getHolder().addCallback( this );
 
 		// make the GamePanel focusable so it can handle events
-		setFocusable( true );
+		this.setFocusable( true );
 
-		world = new World( Physics.GRAVITY_VEC, Physics.DO_SLEEP );
+		this.world = new World( Physics.GRAVITY_VEC, Physics.DO_SLEEP );
+		
+		this.runner = new Runner( this.world );
+		
+		this.platformController = new PlatformController( this.world, this.platforms );
+		
+		this.renderer = new RunningGameRenderer( this, this.platforms, this.runner );
 
-		this.runner = new Runner( world );
-		this.platformRenderer = new PlatformRenderer( world );
-
-		mainThread = new MainThread( getHolder(), this );
+		this.mainThread = new MainThread( this.getHolder(), this );
 	}
 
-	public void update()
+	public void update() 
 	{
-		world.step( 1.0f / 60.0f, 6, 2 );
+		this.world.step( 1.0f / 60.0f, 6, 2 );
+		this.platformController.update();
 		this.runner.update();
-		this.platformRenderer.update();
 	}
 
+	public void render( Canvas canvas )
+	{
+		this.renderer.render( canvas );
+	}
+	
 	@Override
 	public void surfaceChanged( SurfaceHolder holder, int format, int width,
 			int height )
@@ -58,8 +77,8 @@ public class MainGamePanel extends SurfaceView implements
 	@Override
 	public void surfaceCreated( SurfaceHolder arg0 )
 	{
-		mainThread.setRunning( true );
-		mainThread.start();
+		this.mainThread.setRunning( true );
+		this.mainThread.start();
 	}
 
 	@Override
@@ -99,28 +118,5 @@ public class MainGamePanel extends SurfaceView implements
 		}
 		return true;
 	}
-
-	public void setAvgFps( String avgFps )
-	{
-		this.avgFps = avgFps;
-	}
-
-	protected void render( Canvas canvas )
-	{
-		canvas.drawColor( Color.GREEN );
-		displayFps( canvas, avgFps );
-		platformRenderer.draw( canvas );
-		runner.draw( canvas );
-	}
-
-	private void displayFps( Canvas canvas, String fps )
-	{
-		if( canvas != null && fps != null )
-		{
-			Paint paint = new Paint();
-			paint.setARGB( 255, 255, 255, 255 );
-			canvas.drawText( fps, this.getWidth() - 50, 20, paint );
-		}
-	}
-
+	
 }
