821605f679641c664a0b93a2d77b42a01b1f871f
Adam Berg <adam@xyzdigital.com>
2013-02-18 17:54:39 -0800

    Physics class to hold some constants

---
+++ src/com/ajgames/endless_runner/Physics.java
@@ -0,0 +1,19 @@
+package com.ajgames.endless_runner;
+
+import org.jbox2d.common.Vec2;
+
+public class Physics
+{
+	public static final float DRAW_SCALE = 30.0f;
+	public static final Vec2 GRAVITY_VEC = new Vec2( 0.0f, 10.0f );
+	public static final boolean DO_SLEEP = true;
+	public static final float DEFAULT_FRICTION = 0.5f;
+	public static final float DEFAULT_RESTITUTION = 0.3f;
+	public static final float DEFAULT_DENSITY = 1.0f;
+
+	public Physics()
+	{
+
+	}
+
+}
