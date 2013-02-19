55924d05931d2e6335b8f10588e8a890c1c9d501
Adam Berg <adam@xyzdigital.com>
2013-02-18 17:53:10 -0800

    Add constructor with x, y, width, height

--- src/com/ajgames/endless_runner/Sprite.java
+++ src/com/ajgames/endless_runner/Sprite.java
@@ -9,60 +9,68 @@ public class Sprite
 	protected float y;
 	protected int width;
 	protected int height;
-	
+
 	public int getHeight()
 	{
 		return height;
 	}
-	
+
 	public void setHeight( int h )
 	{
 		this.height = h;
 	}
-	
+
 	public int getWidth()
 	{
 		return this.width;
 	}
-	
+
 	public void setWidth( int w )
 	{
 		this.width = w;
 	}
-	
+
 	public float getX()
 	{
 		return this.x;
 	}
-	
+
 	public void setX( float x )
 	{
 		this.x = x;
 	}
-	
+
 	public float getY()
 	{
 		return this.y;
 	}
-	
+
 	public void setY( float y )
 	{
 		this.y = y;
 	}
-	
+
 	public Bitmap getBitmap()
 	{
 		return this.bitmap;
 	}
-	
+
 	public void setBitmap( Bitmap bitmap )
 	{
 		this.bitmap = bitmap;
 	}
-	
+
 	public Sprite()
 	{
-		
+
+	}
+
+	public Sprite( float x, float y, int width, int height )
+	{
+		this.x = x;
+		this.y = y;
+		this.width = width;
+		this.height = height;
 	}
 
 }
