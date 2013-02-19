a039d74d8f0cbca5c17f6ae5cd8326333b89c2ca
Adam Berg <adam@xyzdigital.com>
2013-02-18 17:52:35 -0800

    Initialize the Runner as a box2d dynamic body
    This allows the runner to be affected by other objects and gravity.

--- src/com/ajgames/endless_runner/Runner.java
+++ src/com/ajgames/endless_runner/Runner.java
@@ -1,14 +1,36 @@
 package com.ajgames.endless_runner;
 
-import android.graphics.Bitmap;
+import org.jbox2d.common.Vec2;
+import org.jbox2d.dynamics.BodyType;
+import org.jbox2d.dynamics.World;
 
-
-public class Runner extends AnimatedSprite
+public class Runner extends PhysicsSprite
 {
+	private static final Vec2 JUMP_VECTOR = new Vec2( 0.0f, -100.0f );
+	private static final Vec2 MOVE_RIGHT_VECTOR = new Vec2( 50.0f, 0.0f );
+	private static final Vec2 MOVE_LEFT_VECTOR = new Vec2( -50.0f, 0.0f );
 
-	public Runner( Bitmap bitmap )
+	public boolean movingLeft = false;
+	public boolean movingRight = false;
+
+	public Runner( World world )
 	{
-		super( bitmap, 0, 0, 42, 44, 10, 9 );
+		super( 200.0f, 0.0f, 20, 20, world );
+		this.createBox( BodyType.DYNAMIC, Physics.DEFAULT_DENSITY,
+				Physics.DEFAULT_FRICTION, 0 );
+	}
+
+	public void update()
+	{
+		if( this.x <= 0 && this.body.getLinearVelocity().x < 0 )
+			this.body.applyForce( MOVE_RIGHT_VECTOR, this.body.getPosition() );
+		else if( this.x >= 350 && this.body.getLinearVelocity().x > 0 )
+			this.body.applyForce( MOVE_LEFT_VECTOR, this.body.getPosition() );
+	}
+
+	public void jump()
+	{
+		this.body.applyForce( JUMP_VECTOR, this.body.getPosition() );
 	}
 
 }
