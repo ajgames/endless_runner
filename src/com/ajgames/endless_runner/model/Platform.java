d0280dd3d5b7667a8fe185945e05d0607c78da43
Adam Berg <adam@xyzdigital.com>
2013-02-19 19:30:27 -0800

    Add Platforms to MainCategory for collision filters
    By giving the Platforms a category, we can specify which objects collide
    with this category.  This allows the player to move through the
    platforms when moving upwards.

--- src/com/ajgames/endless_runner/model/Platform.java
+++ src/com/ajgames/endless_runner/model/Platform.java
@@ -2,6 +2,7 @@ package com.ajgames.endless_runner.model;
 
 import org.jbox2d.common.Vec2;
 import org.jbox2d.dynamics.BodyType;
+import org.jbox2d.dynamics.Filter;
 import org.jbox2d.dynamics.World;
 
 
@@ -14,6 +15,10 @@ public class Platform extends PhysicsSprite
 		super( x, y, width, height, world );
 		this.createBox( BodyType.KINEMATIC );
 		this.body.setLinearVelocity( new Vec2( xSpeed, 0 ) );
+		Filter filter = new Filter();
+		filter.categoryBits = Collision.MAIN_CATEGORY;
+		filter.maskBits = Collision.MAIN_CATEGORY;
+		this.body.getFixtureList().setFilterData( filter );
 	}
 
 }
