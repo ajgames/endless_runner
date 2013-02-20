c46bcf6f6c6877df726e2683096566d2b8786e9c
Adam Berg <adam@xyzdigital.com>
2013-02-19 19:31:12 -0800

    Add collision filters for the runner
    These filters allow the runner to pass through platforms when moving
    upwards and collide with them when moving downwards.

--- src/com/ajgames/endless_runner/model/Runner.java
+++ src/com/ajgames/endless_runner/model/Runner.java
@@ -2,26 +2,33 @@ package com.ajgames.endless_runner.model;
 
 import org.jbox2d.common.Vec2;
 import org.jbox2d.dynamics.BodyType;
+import org.jbox2d.dynamics.Filter;
 import org.jbox2d.dynamics.World;
 
 
 public class Runner extends PhysicsSprite
 {
+	private static final int AVOID_PLATFORMS_BITS = 0x0002;
+	
 	private static final Vec2 JUMP_VECTOR = new Vec2( 0.0f, -100.0f );
 	private static final Vec2 MOVE_RIGHT_VECTOR = new Vec2( 50.0f, 0.0f );
 	private static final Vec2 MOVE_LEFT_VECTOR = new Vec2( -50.0f, 0.0f );
 
 	public float speed = -5.0f;
 	
-	public boolean movingLeft = false;
-	public boolean movingRight = false;
+	private boolean movingUp = false;
+	private boolean movingDown = false;
+	
+	private Filter collidePlatformsFilter;
+	private Filter avoidPlatformsFilter;
 
 	public Runner( float startX, float startY, World world )
 	{
 		super( startX, startY, 20, 20, world );
 		this.createBox( BodyType.DYNAMIC, Physics.DEFAULT_DENSITY,
 				0, 0, true );
-		this.bodyDef.fixedRotation = true;
+		
+		this.initFilters();
 	}
 
 	public void update()
@@ -30,6 +37,19 @@ public class Runner extends PhysicsSprite
 			this.body.applyForce( MOVE_RIGHT_VECTOR, this.body.getPosition() );
 		else if( this.getX() >= 310 && this.getLinearVelocity().x > 0 )
 			this.body.applyForce( MOVE_LEFT_VECTOR, this.body.getPosition() );
+		
+		if( this.getLinearVelocity().y > 0 && !this.movingDown )
+		{
+			this.movingUp = false;
+			this.movingDown = true;
+			this.body.getFixtureList().setFilterData( this.collidePlatformsFilter );
+		}
+		else if( this.getLinearVelocity().y < 0 && !this.movingUp )
+		{
+			this.movingUp = true;
+			this.movingDown = false;
+			this.body.getFixtureList().setFilterData( this.avoidPlatformsFilter );
+		}
 	}
 
 	public void jump()
@@ -37,4 +57,15 @@ public class Runner extends PhysicsSprite
 		this.body.applyForce( JUMP_VECTOR, this.body.getPosition() );
 	}
 
+	private void initFilters()
+	{
+		this.avoidPlatformsFilter = new Filter();
+		this.avoidPlatformsFilter.categoryBits = Collision.NO_COLLISION;
+		this.avoidPlatformsFilter.maskBits = Collision.NO_COLLISION;
+		
+		this.collidePlatformsFilter = new Filter();
+		this.collidePlatformsFilter.categoryBits = Collision.MAIN_CATEGORY;
+		this.collidePlatformsFilter.maskBits = Collision.MAIN_CATEGORY;
+	}
+	
 }
