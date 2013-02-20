bb70cae79c04111d01496d2b5f73afcfbd6ef191
Adam Berg <adam@xyzdigital.com>
2013-02-19 17:13:43 -0800

    Update default friction and restitution values

--- src/com/ajgames/endless_runner/model/Physics.java
+++ src/com/ajgames/endless_runner/model/Physics.java
@@ -7,8 +7,8 @@ public class Physics
 	public static final float DRAW_SCALE = 30.0f;
 	public static final Vec2 GRAVITY_VEC = new Vec2( 0.0f, 10.0f );
 	public static final boolean DO_SLEEP = true;
-	public static final float DEFAULT_FRICTION = 0.5f;
-	public static final float DEFAULT_RESTITUTION = 0.3f;
+	public static final float DEFAULT_FRICTION = 0.0f;
+	public static final float DEFAULT_RESTITUTION = 0.0f;
 	public static final float DEFAULT_DENSITY = 1.0f;
 
 	public Physics()
