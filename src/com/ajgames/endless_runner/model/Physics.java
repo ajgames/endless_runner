067259a01ec00371841ef6116d9e9cdfaae73500
Adam Berg <adam@xyzdigital.com>
2013-02-20 14:23:46 -0800

    Change the default gravity value
    Now that you can hold jumps this allows for shorter jumps

--- src/com/ajgames/endless_runner/model/Physics.java
+++ src/com/ajgames/endless_runner/model/Physics.java
@@ -5,7 +5,7 @@ import org.jbox2d.common.Vec2;
 public class Physics
 {
 	public static final float DRAW_SCALE = 30.0f;
-	public static final Vec2 GRAVITY_VEC = new Vec2( 0.0f, 10.0f );
+	public static final Vec2 DEFAULT_GRAVITY_VEC = new Vec2( 0.0f, 25.0f );
 	public static final boolean DO_SLEEP = true;
 	public static final float DEFAULT_FRICTION = 0.0f;
 	public static final float DEFAULT_RESTITUTION = 0.0f;
