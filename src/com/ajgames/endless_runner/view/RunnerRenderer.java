2856361a84c4d6856f46c1c985573fcca2d06405
Adam Berg <adam@xyzdigital.com>
2013-02-21 15:08:39 -0800

    Make RunnerRenderer extend AnimatedSpriteRenderer

--- src/com/ajgames/endless_runner/view/RunnerRenderer.java
+++ src/com/ajgames/endless_runner/view/RunnerRenderer.java
@@ -4,12 +4,12 @@ import android.graphics.Bitmap;
 
 import com.ajgames.endless_runner.model.PhysicsSprite;
 
-public class RunnerRenderer extends BitmapRenderer
+public class RunnerRenderer extends AnimatedSpriteRenderer
 {
 
-	public RunnerRenderer( PhysicsSprite sprite, Bitmap bitmap )
+	public RunnerRenderer( PhysicsSprite sprite, Bitmap bitmap, Animator animator )
 	{
-		super( sprite, bitmap );
+		super( sprite, bitmap, animator, 6, 5, 30, 30 );
 	}
 
 }
